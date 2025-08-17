08bad8f62ba9bd7cfc606023d0939c06adfcc109
Takeru O'oyama <tqer39@gmail.com>
2025-08-17 13:25:47 +0900

    Fix type hint for credentials loading in GmailNotifier (#46)

--- src/gmail_notifier.py
+++ src/gmail_notifier.py
@@ -42,7 +42,7 @@ class GmailNotifier:
 		if missing_padding:
 			token_string += '=' * (4 - missing_padding)
 		token_data = base64.b64decode(token_string.encode('utf-8'))
-		creds = pickle.loads(token_data)
+		creds = pickle.loads(token_data)  # type: Credentials
 
 		# Check if credentials are expired and refresh if possible
 		if not creds.valid:
